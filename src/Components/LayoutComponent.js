a2968c800674fe35015290e5cb89a588e27ae9e5
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2022-04-13 18:23:19 +0200

    Created page when the user is not logged in

--- src/Components/LayoutComponent.js
+++ src/Components/LayoutComponent.js
@@ -1,10 +1,10 @@
-
 import axios from "axios";
 import { useContext } from "react";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthProviderWrapper";
 import { API_BASE_URL } from "../consts";
 import logoIcon from '../Logo/logoIcon.png'
+import cover_photo from '../Logo/cover_photo.png'
 
 export function LayoutComponent() {
   const { user, removeUserFromContext} = useContext(AuthContext);
@@ -52,7 +52,27 @@ export function LayoutComponent() {
           </>
         ) : (
           // does this work? pass error message along: "Please login to access these pages?"
-          navigate("/")
+          //navigate("/")
+          <>
+            <nav className="navbar">
+                <div className="logoAndIcon">
+                  <img className="icon" src={logoIcon} alt="triviahack logo"/>
+                  <p>TRIVIAHACK</p>
+                </div>
+
+                  <NavLink to="/">
+                  Login
+                  {/* &#160;Play */}</NavLink>
+
+                  <NavLink to="/signup">
+                  Sign Up
+                  {/* &#160;Profile */}</NavLink>
+              </nav>
+              <div className="page404">
+                <img src={cover_photo} alt="triviahack logo"/>
+                <h3>You need to be logged in to play our game!</h3>
+              </div>
+          </>
         )
 
       }
