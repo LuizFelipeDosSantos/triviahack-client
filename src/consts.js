1e4214750c6ae743e043d410f0e5e8f6ada6d353
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2022-04-13 21:48:06 +0200

    Trying to deploy

--- src/consts.js
+++ src/consts.js
@@ -4,7 +4,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5005";
+  process.env.REACT_APP_API_BASE_URL;// || "http://localhost:5005";
 
 export async function getCsrfToken() {
   const { data } = await axios.get(API_BASE_URL + "/getCsrfToken");
